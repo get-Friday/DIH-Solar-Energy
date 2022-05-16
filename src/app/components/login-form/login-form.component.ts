d409eb3a7934efc6de9d0a44ded77300e3ebf1a0
get-Friday <gabi.get@hotmail.com>
2022-05-16 19:52:32 -0300

    login validation

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import LOGIN_USER from 'src/app/constants/login';
 
 @Component({
   selector: 'DIH-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
   email: string;
   password: string;
+  failedLogin: boolean = false;
 
-  constructor() { }
+  constructor(private route: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onSubmit() {
+  validate() {
+    if (
+      this.email == LOGIN_USER.username &&
+      this.password == LOGIN_USER.password
+    ) {
+      this.route.navigateByUrl('dashboard');
+    } else {
+      this.failedLogin = true;
+    }
+
+    // localStorage pra não pedir login toda vez
+    // comparar com uma constante de usuário e.g. admin admin
   }
 }
---
+++ src/app/constants/login.ts
@@ -0,0 +1,6 @@
+const LOGIN_USER = {
+  username: 'admin@email.com',
+  password: 'admin'
+}
+
+export default LOGIN_USER

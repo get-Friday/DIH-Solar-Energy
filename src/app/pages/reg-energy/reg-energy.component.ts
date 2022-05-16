a7a573a7dd6e594e7af98bcf558d3f7bee0d6832
get-Friday <gabi.get@hotmail.com>
2022-05-16 16:38:34 -0300

    nav component + units and reg-energy page

---
+++ src/app/pages/reg-energy/reg-energy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'DIH-reg-energy',
+  templateUrl: './reg-energy.component.html',
+  styleUrls: ['./reg-energy.component.scss']
+})
+export class RegEnergyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

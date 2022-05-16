dc9e285e19e5162c9106dab50f48936e7b8d437f
get-Friday <gabi.get@hotmail.com>
2022-05-16 16:38:06 -0300

    login screen page + global display flex + router + dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UnitsComponent } from './pages/units/units.component';
+import { RegEnergyComponent } from './pages/reg-energy/reg-energy.component';
+import { LoginComponent } from './pages/login/login.component';
+
+const ROUTES: Route[] = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'units',
+    component: UnitsComponent,
+  },
+  {
+    path: 'energy',
+    component: RegEnergyComponent,
+  },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    NavComponent,
+    DashboardComponent,
+    UnitsComponent,
+    RegEnergyComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'DIH-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  styles: ['@import url("https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400&display=swap");']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

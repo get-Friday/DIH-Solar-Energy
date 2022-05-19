928f5b0d4cde8af11b667760bc206f4c9614163a
get-Friday <gabi.get@hotmail.com>
2022-05-19 20:30:52 -0300

    new unit reset form

--- src/app/modules/units/new/new.component.ts
+++ src/app/modules/units/new/new.component.ts
@@ -3,35 +3,42 @@ import { NgForm } from '@angular/forms';
 import IUnits from 'src/app/models/units.model';
 import { UnitsService } from 'src/app/services/units.service';
 
+
 @Component({
   selector: 'DIH-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
-export class NewComponent implements OnInit, IUnits {
-  id: number;
-  alias: string;
-  located: string;
-  brand: string;
-  model: number;
-  active: boolean;
+export class NewComponent implements OnInit {
+  unit: IUnits = {
+    id: 0,
+    alias: '',
+    located: '',
+    brand: '',
+    active: false,
+  };
 
-  constructor(private unitsService: UnitsService) { }
+  constructor(private unitsService: UnitsService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(newUnit: NgForm) {
     let unitObject: IUnits = newUnit.value;
     unitObject = {
       id: Math.floor(Math.random() * 1000),
-      ...unitObject
-    }
+      ...unitObject,
+    };
+    this.unitsService.addUnit(unitObject);
+  }
 
-    this.unitsService.addUnit(unitObject)
-      .subscribe(arg => {
-        console.log(arg)
-      });
+  resetForm() {
+    this.unit = {
+      id: 0,
+      alias: '',
+      located: '',
+      brand: '',
+      active: false,
+    };
   }
 }

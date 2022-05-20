2e5d2b890cf5851f332417637a7779602daa57b1
get-Friday <gabi.get@hotmail.com>
2022-05-20 20:49:17 -0300

    add functionality to cards on dashboard

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { EnergyService } from 'src/app/services/energy.service';
+import { UnitsService } from 'src/app/services/units.service';
 
 @Component({
   selector: 'DIH-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  unitsTotal: number = 0;
+  activeUnits: number = 0;
+  inactiveUnits: number = 0;
+  averageEnergy: number = 0;
 
-  constructor() { }
+  constructor(
+    private unitsService: UnitsService,
+    private energyService: EnergyService
+  ) {}
 
   ngOnInit(): void {
+    this.unitsService.getUnits().subscribe((arg) => {
+      this.unitsTotal = arg.length;
+      this.activeUnits = arg.filter((unit) => unit.active).length;
+      this.inactiveUnits = this.unitsTotal - this.activeUnits;
+    });
   }
-
 }

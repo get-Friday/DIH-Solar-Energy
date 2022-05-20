a20c9b867792bdcc41e26b16271bc83bc6f8d1b2
get-Friday <gabi.get@hotmail.com>
2022-05-20 14:16:29 -0300

    page not found and wild card routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 import { LoginComponent } from './pages/login/login.component';
 
@@ -13,6 +14,10 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/admin/admin.module').then((m) => m.AdminModule),
   },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({

a68849ed02d9ee40f84d0733761f3672d84834db
get-Friday <gabi.get@hotmail.com>
2022-05-24 01:03:26 -0300

    auto-format

--- src/app/modules/admin/chart/chart.component.ts
+++ src/app/modules/admin/chart/chart.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { BaseChartDirective } from 'ng2-charts';
 import IEnergy from 'src/app/models/energy.model';
 import { EnergyService } from 'src/app/services/energy.service';
 
 @Component({
   selector: 'DIH-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.scss']
+  styleUrls: ['./chart.component.scss'],
 })
 export class ChartComponent implements OnInit {
   currentDate = new Date();
@@ -14,44 +13,44 @@ export class ChartComponent implements OnInit {
   isReady: boolean = false;
   chartConfig = {
     data: {
-      datasets: [{
-        label: `Energia em ${this.currentDate.getFullYear()}`,
-        data: [],
-        fill: false,
-        borderColor: '#2196F3',
-        pointBorderColor: 'black',
-        tension: 0.4
-      }],
-      labels: this.month
-    }
-  }
+      datasets: [
+        {
+          label: `Energia em ${this.currentDate.getFullYear()}`,
+          data: [],
+          fill: false,
+          borderColor: '#2196F3',
+          pointBorderColor: 'black',
+          tension: 0.4,
+        },
+      ],
+      labels: this.month,
+    },
+  };
 
-  constructor(private energyService: EnergyService) { }
+  constructor(private energyService: EnergyService) {}
 
   ngOnInit(): void {
     this.energyService.getEnergy().subscribe((energy) => {
-      const entries: IEnergy[] = energy;
-      this.setXAxisRange()
-      this.chartConfig.data.datasets[0].data = this.setData(entries);
+      this.setXAxisRange();
+      this.chartConfig.data.datasets[0].data = this.setData(energy);
       this.isReady = true;
-    })
+    });
   }
 
   setData(data: IEnergy[]): number[] {
     const currentMonth = this.currentDate.getMonth();
     const currentYear = this.currentDate.getFullYear();
-    const totalKw = []
+    const totalKw = [];
 
-    for(let i = 1; i <= currentMonth + 1 ; i++){
-      let monthTemplate = ``
-      i < 10 ? monthTemplate = `0${i}` : monthTemplate = `${i}`
+    for (let i = 1; i <= currentMonth + 1; i++) {
+      let monthTemplate = ``;
+      i < 10 ? (monthTemplate = `0${i}`) : (monthTemplate = `${i}`);
 
-      const sameMonthData = data.filter(e => e.date == `${currentYear}-${monthTemplate}`)
-      const totalKwMonth = sameMonthData.reduce((p, c) =>
-        p + c.totalKw,
-        0
-      )
-      totalKw.push(totalKwMonth)
+      const sameMonthData = data.filter(
+        (e) => e.date == `${currentYear}-${monthTemplate}`
+      );
+      const totalKwMonth = sameMonthData.reduce((p, c) => p + c.totalKw, 0);
+      totalKw.push(totalKwMonth);
     }
 
     return totalKw;
